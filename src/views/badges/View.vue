b944984a6a3156531125873c5bd8e066430fa01a
rendyprayaoga <rendyprayoga806@gmail.com>
2024-05-26 17:56:04 +0700

    add badges views

---
+++ src/views/badges/View.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <BadgeCard />
+  </div>
+</template>
+<script>
+import BadgeCard from '../../components/badges/BadgeCard';
+export default {
+  components: { BadgeCard },
+};
+</script>
