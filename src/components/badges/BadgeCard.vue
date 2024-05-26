5a467bc9aff390347a732837e5ba4ee06ac1e906
rendyprayaoga <rendyprayoga806@gmail.com>
2024-05-26 17:48:29 +0700

    add file badges

---
+++ src/components/badges/BadgeCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="card"
+    style="width: 15rem; background-color: #e8c872; border-radius: auto"
+  >
+    <div class="card-body">
+      <h5 class="card-title" style="text-align: center; margin-top: 6px">
+        tes
+      </h5>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
