6668ff60784765029643abe003a74274003764b7
rendyprayaoga <rendyprayoga806@gmail.com>
2024-05-30 21:37:05 +0700

    update view badge

--- src/views/courses/View.vue
+++ src/views/courses/View.vue
@@ -24,17 +24,6 @@ export default {
         this.isFetching = false;
       }
     },
-    // getCategory() {
-    //   console.log('getCategory');
-    //   this.fetch();
-    //   this.clicked = true;
-    // },
-    // push() {
-    //   let query = { ...this.$route.query, ...this.input };
-    //   query = pickBy(query, (value) => value !== '');
-
-    //   this.$router.replace({ query });
-    // },
   },
   created() {
     this.fetch();
