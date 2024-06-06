593279bd7a93e9d9874c690f8bd3c3cb21d4d486
rendyprayaoga <rendyprayoga806@gmail.com>
2024-06-06 17:01:33 +0700

    update search

--- src/components/reports/ReportTable.vue
+++ src/components/reports/ReportTable.vue
@@ -1,25 +1,18 @@
 <template>
   <div>
     <!-- Dropdown to select category -->
-    <div class="d-flex flex-wrap  mb-md-2"">
-      <div class="me-auto" >
-      <label for="category">Category</label>
-      <select
-        id="category"
-        class="form-control"
-        v-model="selectedCategory"
-        @change="onCategoryChange"
-      >
-        <option value="">All</option>
-        <!-- Untuk menampilkan semua data tanpa filter -->
-        <option
-          v-for="category in categories"
-          :key="category.id"
-          :value="category.id"
-        >
-          {{ category.name }}
-        </option>
-      </select>
+    <div class="d-flex flex-wrap mb-md-2">
+      <div class="me-auto">
+        <label for="category">Category</label>
+        <select id="category" class="form-control" v-model="input.categoryId">
+          <option
+            v-for="category in categories"
+            :key="category.id"
+            :value="category.id"
+          >
+            {{ category.name }}
+          </option>
+        </select>
       </div>
     </div>
 
@@ -32,24 +25,23 @@
             <th scope="col">Name</th>
             <th scope="col">Score</th>
             <th scope="col">Category</th>
-            <th scope="col">Date</th>
+            <!-- <th scope="col">Date</th> -->
           </tr>
         </thead>
-        <tbody style="background-color: #e7b325;">
-          <tr v-for="(data, index) in filteredSortedReport" :key="data.id">
+        <tbody style="background-color: #e7b325">
+          <tr v-for="(data, index) in report" :key="data.id">
             <td>{{ index + 1 }}</td>
-             <!-- <td>{{ data.rank }}</td> -->
             <td>{{ data.User.name }}</td>
             <td>{{ data.score }}</td>
             <td>{{ data.Category.name }}</td>
-            <td>{{ moment(data.createdAt).format('DD-MM-YYYY HH:mm:ss') }}</td>
+            <!-- <td>{{ moment(data.createdAt).format('DD-MM-YYYY HH:mm:ss') }}</td> -->
           </tr>
         </tbody>
       </table>
     </div>
 
-    <TableLoading v-if="!filteredSortedReport.length && isFetching" />
-    <TableNoData v-else-if="!filteredSortedReport.length" />
+    <TableLoading v-if="!report.length && isFetching" />
+    <TableNoData v-else-if="!report.length" />
 
     <Pagination
       v-if="meta && meta.lastPage > 1"
@@ -79,68 +71,43 @@ export default {
     return {
       isFetching: false,
       report: [],
-      categories: [], // Untuk menyimpan daftar kategori
+      categories: [],
       meta: null,
-      selectedCategory: '', // Untuk menyimpan kategori yang dipilih
+      input: {
+        categoryId: '', // Untuk menyimpan kategori yang dipilih
+      },
     };
-  },
-
-  computed: {
-     filteredSortedReport() {
-      // Filter data berdasarkan kategori yang dipilih
-      let data = this.report;
-      if (this.selectedCategory !== '') {
-        data = data.filter(
-          (item) => item.Category.id === this.selectedCategory
-        );
-      }
-
-      // Sortir berdasarkan skor dan penilaian rank
-      data = data.sort((a, b) => b.score - a.score);
-      let rank = 1;
-
-      return data.map((item, index, arr) => {
-        if (index > 0 && arr[index - 1].score > item.score) {
-          rank = index + 1;
-        }
-        return {
-          ...item,
-          rank,
-        };
-      });
-    },
   },
 
   methods: {
     async fetch() {
       try {
         this.isFetching = true;
+
         const [reportResult, categoryResult] = await Promise.all([
           Report.fetch(this.$route.query),
-          Category.fetch(), // Mendapatkan semua kategori
+          Category.fetch(),
         ]);
+
         this.report = reportResult.data;
         this.meta = reportResult.meta;
-        this.categories = categoryResult.data; // Simpan daftar kategori
+        this.categories = categoryResult.data;
       } catch (error) {
-        console.error('Error fetching data:', error); // Debug jika ada kesalahan
+        console.error('Error fetching data:', error);
       } finally {
         this.isFetching = false;
       }
     },
-
-    onCategoryChange() {
-      // Fetch lagi dengan query kategori yang dipilih
-      this.fetch();
-    },
   },
 
   watch: {
-    '$route.query': {
-      handler() {
-        this.fetch();
+    'input.categoryId': {
+      handler(value) {
+        if (value) {
+          this.$route.query.categoryId = value;
+          this.fetch();
+        }
       },
-      deep: true,
     },
   },
 
